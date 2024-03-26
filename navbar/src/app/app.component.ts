8ba8614be23e9dd68585d26f5cccc9ea8bf41d42
LUSSIER Romain <Romain.LUSSIER@fgtb.be>
2024-03-26 13:14:06 +0100

    first iteration of MFE navbar - not working yet

--- navbar/src/app/app.component.ts
+++ navbar/src/app/app.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { registerApplication, start } from 'single-spa';
 
+interface Application {
+  name: string;
+  appURL: string;
+  route: string;
+}
 
 @Component({
   selector: 'navbar-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'navbar';
+export class AppComponent implements OnInit {
+  title = 'FGTB Navbar';
+  applications: Application[] = []; // Add this line
+
+  ngOnInit() {
+    fetch('applications.json')
+      .then(res => res.json())
+      .then(apps => {
+        this.applications = apps; // Store fetched apps
+        apps.forEach(app => {
+          registerApplication({
+            name: app.name,
+            app: () => import(app.appURL),
+            activeWhen: app.route
+          });
+        });
+
+        start(); // Start single-spa
+      })
+      .catch(error => {
+        console.error("Failed to register micro frontends", error);
+      });
+  }
 }
